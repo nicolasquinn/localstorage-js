ac247c7b5e98412494d1e6a6f3872b2ab91ee9b3
Nicolas Quinn <nclsquinn@gmail.com>
2022-09-06 22:34:18 -0300

    148 - Validando el formulario

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variables
 const formulario = document.querySelector('#formulario');
 const listaTweets = document.querySelector('#lista-tweets');
+const contenido = document.querySelector('#contenido');
 let tweets = [];
 
 // Eventos
@@ -10,7 +11,39 @@ function eventListeners () {
 }
 
 // Funciones
-
 function agregarTweet (e) {
     e.preventDefault();
+
+    // Almaceno el texto escrito en el textarea
+    const tweet = document.querySelector('#tweet').value;
+
+    // Validación
+    if (tweet.length === 0) {
+        mostrarError('El tweet no puede estar vacío.')
+    } else {
+        console.log("tiene algo.")
+    }
+    
+}
+
+// Función reutilizable para mostrar mensajes de error.
+function mostrarError (mensaje) {
+
+    // Verifico si existe un error, si no existe, lo creo.
+    if (contenido.children.length === 1) {
+        const errorMsg = document.createElement('P');
+        errorMsg.textContent = mensaje;
+        errorMsg.classList.add('error');
+        contenido.appendChild(errorMsg)
+        setTimeout(() => {
+            eliminarError();
+        }, 5000);
+    }
+
+}
+
+// Función reutilizable para borrar los mensajes de error.
+function eliminarError() {
+    const elHijo = contenido.children[1];
+    elHijo.remove();
 }
